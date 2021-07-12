64d4cda141f6756759f01221e50978a045d92c1b
suneettipirneni <suneettipirneni@icloud.com>
2021-07-12 01:17:07 -0400

    revert

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { default as Command, isCommand } from './command';
 export { default as CommandManager } from './commandManager';
-export type { default as Client } from './client';
+export { default as Client } from './client';
 export { default as Dispatchable } from './dispatcher';
 export { default as MessageDispatcher } from './commandDispatch';
