d6d7f5ddd40596e8014d1e87b5af1c060374235b
Robert Boyd III <rob@robertcboyd.com>
2021-07-11 20:10:42 -0400

    Use reexports instead of duplication

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { default as Command, isCommand } from './command';
+export { default as CommandManager } from './commandManager';
+export { default as Client } from './client';
+export { default as Dispatchable } from './dispatcher';
+export { default as MessageDispatcher } from './commandDispatch';
