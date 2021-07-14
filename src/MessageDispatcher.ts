84b8e89e37d94d73b4730329065e6c4846ecbb9f
suneettipirneni <suneettipirneni@icloud.com>
2021-07-14 12:18:07 -0400

    clean up code and fix loop

--- src/MessageDispatcher.ts
+++ src/MessageDispatcher.ts
@@ -1,8 +1,9 @@
 import {
-  CommandInteraction, GuildMember
+  CommandInteraction,
 } from 'discord.js';
 import CommandManager from './CommandManager';
 import Dispatchable from './Dispatchable';
+import { hasRoles, isInChannels } from './util';
 
 export default class MessageDispatcher implements Dispatchable {
   constructor(private readonly manager: CommandManager) {}
@@ -15,10 +16,9 @@ export default class MessageDispatcher implements Dispatchable {
       interaction.reply('Error finding that command');
       return;
     }
-
     // Check channel permissions
     if (command.allowedChannels) {
-      if (!command.allowedChannels.includes(interaction.channelId)) {
+      if (!isInChannels(command.allowedChannels, interaction)) {
         const errMsg =
           'Please use this command in an allowed channel:\n'.concat(
             ...command.allowedChannels.map((channel) => `- <#${channel}>\n`)
@@ -32,27 +32,7 @@ export default class MessageDispatcher implements Dispatchable {
 
     // Check allowed roles.
     if (command.allowedRoles) {
-      let allowedRole = true;
-      const { member } = interaction;
-
-      // If there's no member associated this is not a normal message.
-      if (!member) {
-        return;
-      }
-
-      // Iterate and check if roles are present.
-      if (member instanceof GuildMember) {
-        command.allowedRoles?.forEach(roleID => {
-          allowedRole = member.roles.cache.find(role => role.id === roleID) !== undefined;
-        });
-      } else {
-        command.allowedRoles?.forEach(roleID => {
-          allowedRole = member.roles.includes(roleID);
-        });
-      }
-    
-      // Send error if roles could not be found on user.
-      if (!allowedRole) {
+      if (!hasRoles(command.allowedRoles, interaction.member)) {
         const errMsg =
       'You must have to following roles to run this command:\n'.concat(
         ...command.allowedRoles.map((role) => `- <@&${role}>\n`)
