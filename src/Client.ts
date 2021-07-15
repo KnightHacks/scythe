2e43757964513df49c04c6f4eee230e1e454fa14
suneettipirneni <suneettipirneni@icloud.com>
2021-07-14 22:56:52 -0400

    register conditionally.

--- src/Client.ts
+++ src/Client.ts
@@ -1,4 +1,4 @@
-import discord, { ButtonInteraction, ClientOptions, CommandInteraction } from 'discord.js';
+import discord, { ApplicationCommandData, ButtonInteraction, ClientOptions, CommandInteraction } from 'discord.js';
 import MessageDispatcher from './MessageDispatcher';
 import { CommandLoader } from './CommandLoader';
 import CommandManager from './CommandManager';
@@ -16,24 +16,6 @@ export default class Client extends discord.Client {
     super(options);
     this.commands = new CommandManager(this);
     this.commandDispatch = new MessageDispatcher(this.commands);
-
-    // Register commands on connection
-    this.once('ready', async () => {
-      // Convert commands.
-      const appCommands = this.commands.toAppCommands();
-
-      // Guild commands propogate instantly, but application commands do not
-      // so we only want to use guild commands when in development.
-      if (process.env.NODE_ENV === 'development' && process.env.GUILD_ID) {
-        console.log(
-          'Development environment detected, using guild commands instead of application commands.'
-        );
-        const guild = this.guilds.cache.get(process.env.GUILD_ID);
-        await guild?.commands.set(appCommands);
-      } else {
-        await this.application?.commands.set(appCommands);
-      }
-    });
 
     // Enable dispatcher.
     this.on('interactionCreate', (interaction) => {
@@ -65,6 +47,31 @@ export default class Client extends discord.Client {
 
     // Register all commands.
     commands.forEach((command) => this.commands.register(command));
+
+    // Get discord-compatible commands.
+    const appCommands = this.commands.toAppCommands();
+
+    if (!this.readyAt) {
+      // Register commands to the discord API, once the client is ready.
+      this.once('ready', () => this.pushCommands(appCommands));
+    } else {
+      // If we get here the client is already ready, so we'll register immediately.
+      await this.pushCommands(commands);
+    }
+  }
+
+  private async pushCommands(commands: ApplicationCommandData[]) {
+    // Guild commands propogate instantly, but application commands do not
+    // so we only want to use guild commands when in development.
+    if (process.env.NODE_ENV === 'development' && process.env.GUILD_ID) {
+      console.log(
+        'Development environment detected..., using guild commands instead of application commands.'
+      );
+      const guild = this.guilds.cache.get(process.env.GUILD_ID);
+      await guild?.commands.set(commands);
+    } else {
+      await this.application?.commands.set(commands);
+    }
   }
 
   public addButtonListener(interactionID: string, handler: ButtonHandler): void {
