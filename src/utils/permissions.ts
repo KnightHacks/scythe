b283cf7d91b304bc60c386e91c2ae5b1b4217271
suneettipirneni <suneettipirneni@icloud.com>
2021-07-18 17:45:04 -0400

    change method names

--- src/utils/permissions.ts
+++ src/utils/permissions.ts
@@ -4,131 +4,128 @@ import { resolveRoleID } from './role';
 
 const hasID = (member: GuildMember, id: Snowflake) => member.roles.cache.has(id);
 
-export const RolePermissions = {
+/**
+ * A helper function to check if the given roles are present from an interaction.
+ * @param roles The roles names to check for.
+ * @returns A permission handler function.
+ */
+export function allRoleNames(...roles: string[]): PermissionHandler {
+  return async (interaction) => {
+    let allowed = true;
+    // FIXME is this cast safe?
+    const member = interaction.member as GuildMember;
+    if (!member) {
+      return false;
+    }
 
-  /**
-   * A helper function to check if the given roles are present from an interaction.
-   * @param roles The roles names to check for.
-   * @returns A permission handler function.
-   */
-  allNames(...roles: string[]): PermissionHandler {
-    return async (interaction) => {
-      let allowed = true;
-      // FIXME is this cast safe?
-      const member = interaction.member as GuildMember;
-      if (!member) {
+    roles.forEach(
+      (roleName) =>
+        (allowed &&= member.roles.cache.some((role) => role.name === roleName))
+    );
+
+    // Iterate and check if roles are present.
+    if (!allowed) {
+      const guild = interaction.guild;
+      if (!guild) {
+        console.log(new Error('Could not find guild'));
         return false;
       }
-
-      roles.forEach(
-        (roleName) =>
-          (allowed &&= member.roles.cache.some((role) => role.name === roleName))
+      const roleIDs = roles.map((role) => resolveRoleID(guild, role));
+      const errMsg =
+      'You must have the following roles to run this command:\n'.concat(
+        ...roleIDs.map((role) => `- <@&${role}>\n`)
       );
+      await interaction.reply({ content: errMsg, ephemeral: true });
+    }
 
-      // Iterate and check if roles are present.
-      if (!allowed) {
-        const guild = interaction.guild;
-        if (!guild) {
-          console.log(new Error('Could not find guild'));
-          return false;
-        }
-        const roleIDs = roles.map((role) => resolveRoleID(guild, role));
-        const errMsg =
-        'You must have the following roles to run this command:\n'.concat(
-          ...roleIDs.map((role) => `- <@&${role}>\n`)
-        );
-        await interaction.reply({ content: errMsg, ephemeral: true });
-      }
+    return allowed;
+  };
+}
 
-      return allowed;
-    };
-  },
+/**
+ * A helper function to check if the sender has one of the given roles.
+ * @param roles The roles names to check for.
+ * @returns A permission handler function.
+ */
+export function oneRoleName(...roles: string[]): PermissionHandler {
+  return async (interaction) => {
+    // FIXME is this cast safe?
+    const member = interaction.member as GuildMember;
+    const allowed = roles.some((roleName) =>
+      member.roles.cache.find((role) => role.name === roleName)
+    );
 
-  /**
-   * A helper function to check if the sender has one of the given roles.
-   * @param roles The roles names to check for.
-   * @returns A permission handler function.
-   */
-  oneName(...roles: string[]): PermissionHandler {
-    return async (interaction) => {
-      // FIXME is this cast safe?
-      const member = interaction.member as GuildMember;
-      const allowed = roles.some((roleName) =>
-        member.roles.cache.find((role) => role.name === roleName)
-      );
-
-      if (!allowed) {
-        const guild = interaction.guild;
-        if (!guild) {
-          console.log(new Error('Could not find guild!'));
-          return false;
-        }
-        const roleIDs = roles.map((role) => resolveRoleID(guild, role));
-        const errMsg =
-        'You must have one of the following roles to run this command:\n'.concat(
-          ...roleIDs.map((role) => `- <@&${role}>\n`)
-        );
-        await interaction.reply({ content: errMsg, ephemeral: true });
-      }
-
-      return allowed;
-    };
-  },
-
-  /**
-   * A helper function to check if the given roles are present from an interaction.
-   * @param roles The roles IDs to check for.
-   * @returns A permission handler function.
-   */
-  allIDs(...roleIDs: Snowflake[]): PermissionHandler {
-    return async (interaction) => {
-      const member = interaction.member as GuildMember;
-      let allowed = true;
-      if (!member) {
+    if (!allowed) {
+      const guild = interaction.guild;
+      if (!guild) {
+        console.log(new Error('Could not find guild!'));
         return false;
       }
+      const roleIDs = roles.map((role) => resolveRoleID(guild, role));
+      const errMsg =
+      'You must have one of the following roles to run this command:\n'.concat(
+        ...roleIDs.map((role) => `- <@&${role}>\n`)
+      );
+      await interaction.reply({ content: errMsg, ephemeral: true });
+    }
 
-      roleIDs.forEach(roleID => allowed &&= hasID(member, roleID));
+    return allowed;
+  };
+}
 
-      if (!allowed) {
-        const errMsg =
-        'You must have the following roles to run this command:\n'.concat(
-          ...roleIDs.map((role) => `- <@&${role}>\n`)
-        );
-        await interaction.reply({ content: errMsg, ephemeral: true });
-      }
+/**
+ * A helper function to check if the given roles are present from an interaction.
+ * @param roles The roles IDs to check for.
+ * @returns A permission handler function.
+ */
+export function allRoles(...roleIDs: Snowflake[]): PermissionHandler {
+  return async (interaction) => {
+    const member = interaction.member as GuildMember;
+    let allowed = true;
+    if (!member) {
+      return false;
+    }
+
+    roleIDs.forEach(roleID => allowed &&= hasID(member, roleID));
+
+    if (!allowed) {
+      const errMsg =
+      'You must have the following roles to run this command:\n'.concat(
+        ...roleIDs.map((role) => `- <@&${role}>\n`)
+      );
+      await interaction.reply({ content: errMsg, ephemeral: true });
+    }
+    
+    return allowed;
+  };
+}
+
+/**
+ * A helper function to check if the sender has one of the given roles.
+ * @param roles The roles IDs to check for.
+ * @returns A permission handler function.
+ */
+export function oneRole(...roleIDs: Snowflake[]): PermissionHandler {
+  return async (interaction) => {
+    const member = interaction.member as GuildMember;
+    if (!member) {
+      return false;
+    }
+
+    const allowed = member.roles.cache.some(role => roleIDs.includes(role.id));
+
+    if (!allowed) {
+      const errMsg =
+      'You must have one of the following roles to run this command:\n'.concat(
+        ...roleIDs.map((role) => `- <@&${role}>\n`)
+      );
       
-      return allowed;
-    };
-  },
+      await interaction.reply({ content: errMsg, ephemeral: true });
+    }
 
-  /**
-   * A helper function to check if the sender has one of the given roles.
-   * @param roles The roles IDs to check for.
-   * @returns A permission handler function.
-   */
-  oneID(...roleIDs: Snowflake[]): PermissionHandler {
-    return async (interaction) => {
-      const member = interaction.member as GuildMember;
-      if (!member) {
-        return false;
-      }
-
-      const allowed = member.roles.cache.some(role => roleIDs.includes(role.id));
-
-      if (!allowed) {
-        const errMsg =
-        'You must have one of the following roles to run this command:\n'.concat(
-          ...roleIDs.map((role) => `- <@&${role}>\n`)
-        );
-        
-        await interaction.reply({ content: errMsg, ephemeral: true });
-      }
-
-      return allowed;
-    };
-  }
-};
+    return allowed;
+  };
+}
 
 /**
  * A helper function used to aggregate permission handlers.
@@ -150,126 +147,122 @@ export function checkAll(...handlers: PermissionHandler[]): PermissionHandler {
   };
 }
 
-export const ChannelPermissions = {
-  /**
-   * A helper function to check if the interaction was sent in the given channel(s).
-   * @param channels The channels names to check for.
-   * @returns A permission handler function.
-   */
-  inNames(...channels: string[]): PermissionHandler {
-    return async (interaction) => {
-      // Resolve each of the channel names
-      const channelIDs = channels.map(
-        (channelName) =>
-          interaction.client.channels.cache.find(
-            (channel) =>
-              (<TextChannel | ThreadChannel>channel).name === channelName
-          )?.id
+/**
+ * A helper function to check if the interaction was sent in the given channel(s).
+ * @param channels The channels names to check for.
+ * @returns A permission handler function.
+ */
+export function inChannelNames(...channels: string[]): PermissionHandler {
+  return async (interaction) => {
+    // Resolve each of the channel names
+    const channelIDs = channels.map(
+      (channelName) =>
+        interaction.client.channels.cache.find(
+          (channel) =>
+            (<TextChannel | ThreadChannel>channel).name === channelName
+        )?.id
+    );
+    if (!channelIDs || !interaction.channel) {
+      console.log('No channels were found!');
+      return false;
+    }
+
+    const valid = channelIDs.includes(interaction.channelId);
+
+    if (!valid) {
+      const errMsg = 'Please use this command in an allowed channel:\n'.concat(
+        ...channelIDs.map((channel) => `- <#${channel}>\n`)
       );
-      if (!channelIDs || !interaction.channel) {
-        console.log('No channels were found!');
-        return false;
-      }
 
-      const valid = channelIDs.includes(interaction.channelId);
+      // Send error message.
+      await interaction.reply({ content: errMsg, ephemeral: true });
+    }
 
-      if (!valid) {
-        const errMsg = 'Please use this command in an allowed channel:\n'.concat(
-          ...channelIDs.map((channel) => `- <#${channel}>\n`)
-        );
+    return valid;
+  };
+}
 
-        // Send error message.
-        await interaction.reply({ content: errMsg, ephemeral: true });
-      }
+/**
+ * A helper function to check if the interaction was sent in the given channel(s).
+ * @param channels The channels IDs to check for.
+ * @returns A permission handler function.
+ */
+export function inChannels(...channelIDs: Snowflake[]): PermissionHandler {
+  return async (interaction) => {
 
-      return valid;
-    };
-  },
+    if (!interaction.channel) {
+      console.log('No channels found');
+    }
 
-  /**
-   * A helper function to check if the interaction was sent in the given channel(s).
-   * @param channels The channels IDs to check for.
-   * @returns A permission handler function.
-   */
-  inIDs(...channelIDs: Snowflake[]): PermissionHandler {
-    return async (interaction) => {
+    const allowed = channelIDs.includes(interaction.channelId);
 
-      if (!interaction.channel) {
-        console.log('No channels found');
-      }
+    if (!allowed) {
+      const errMsg = 'Please use this command in an allowed channel:\n'.concat(
+        ...channelIDs.map((channel) => `- <#${channel}>\n`)
+      );
 
-      const allowed = channelIDs.includes(interaction.channelId);
+      // Send error message.
+      await interaction.reply({ content: errMsg, ephemeral: true });
+    }
 
-      if (!allowed) {
-        const errMsg = 'Please use this command in an allowed channel:\n'.concat(
-          ...channelIDs.map((channel) => `- <#${channel}>\n`)
-        );
+    return allowed;
+  };
+}
 
-        // Send error message.
-        await interaction.reply({ content: errMsg, ephemeral: true });
-      }
+/**
+ * Checks if a given interaction is created in an allowed category.
+ * @param categories The categories names to check for
+ * @returns A permission handler.
+ */
+export function inCategoryNames(...categories: string[]): PermissionHandler {
+  return async (interaction) => {
+    if (!(interaction.channel instanceof TextChannel)) {
+      return false;
+    }
 
-      return allowed;
-    };
-  }
-};
+    const category = interaction.channel.parent?.name;
+    if (!category) {
+      return false;
+    }
 
-export const CategoryPermissions = {
-  /**
-   * Checks if a given interaction is created in an allowed category.
-   * @param categories The categories names to check for
-   * @returns A permission handler.
-   */
-  inNames(...categories: string[]): PermissionHandler {
-    return async (interaction) => {
-      if (!(interaction.channel instanceof TextChannel)) {
-        return false;
-      }
+    const allowed = categories.includes(category);
 
-      const category = interaction.channel.parent?.name;
-      if (!category) {
-        return false;
-      }
+    if (!allowed) {
+      const content =
+      'Please use the command in the following categories:\n'.concat(
+        ...categories.map((cur) => `- ${cur}\n`)
+      );
+      await interaction.reply({ content, ephemeral: true });
+    }
 
-      const allowed = categories.includes(category);
+    return allowed;
+  };
+}
 
-      if (!allowed) {
-        const content =
-        'Please use the command in the following categories:\n'.concat(
-          ...categories.map((cur) => `- ${cur}\n`)
-        );
-        await interaction.reply({ content, ephemeral: true });
-      }
+/**
+ * Checks if a given interaction is created in an allowed category.
+ * @param categories The categories IDs to check for
+ * @returns A permission handler.
+ */
+export function inCategories(...categoryIDs: Snowflake[]): PermissionHandler {
+  return async (interaction) => {
+    if (!(interaction.channel instanceof TextChannel)) {
+      return false;
+    }
 
-      return allowed;
-    };
-  },
+    const category = interaction.channel.parent;
 
-  /**
-   * Checks if a given interaction is created in an allowed category.
-   * @param categories The categories IDs to check for
-   * @returns A permission handler.
-   */
-  inIDs(...categoryIDs: Snowflake[]): PermissionHandler {
-    return async (interaction) => {
-      if (!(interaction.channel instanceof TextChannel)) {
-        return false;
-      }
+    if (!category) {
+      return false;
+    }
 
-      const category = interaction.channel.parent;
+    const allowed = categoryIDs.includes(category.id);
 
-      if (!category) {
-        return false;
-      }
+    if (!allowed) {
+      const content = 'This command is not allowed in this category';
+      await interaction.reply({ content, ephemeral: true });
+    }
 
-      const allowed = categoryIDs.includes(category.id);
-
-      if (!allowed) {
-        const content = 'This command is not allowed in this category';
-        await interaction.reply({ content, ephemeral: true });
-      }
-
-      return allowed;
-    };
-  }
-};
+    return allowed;
+  };
+}
