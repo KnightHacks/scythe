58959eaa25a2f25aabc930f9ae7f5707830eedf0
Robert Boyd III <rob@robertcboyd.com>
2021-07-24 19:36:35 -0400

    Fix bug with link buttons

--- src/UI.ts
+++ src/UI.ts
@@ -1,45 +1,61 @@
 import {
+  EmojiIdentifierResolvable,
   MessageActionRow,
   MessageButtonOptions,
+  MessageButtonStyleResolvable,
 } from 'discord.js';
 import { ButtonHandler } from './ButtonHandler';
 import Client from './Client';
 import { v4 as uuidv4 } from 'uuid';
 
-export type UIComponent = DispatchButton;
+export type UIComponent = DispatchButton | DispatchLinkButton;
 
-export type ButtonOptions = MessageButtonOptions & {
-  onClick?: ButtonHandler;
+export type ButtonOptions = {
+  disabled?: boolean;
+  emoji?: EmojiIdentifierResolvable;
+  label?: string;
+  style: Exclude<MessageButtonStyleResolvable, 'LINK'>;
+  onClick: ButtonHandler;
+};
+
+export type LinkButtonOptions = {
+  disabled?: boolean;
+  emoji?: EmojiIdentifierResolvable;
+  label?: string;
+  url: string;
 };
 
 export class DispatchButton {
   constructor(readonly options: ButtonOptions) {}
 }
 
+export class DispatchLinkButton {
+  constructor(readonly options: LinkButtonOptions) {}
+}
+
 export function toComponents(
   client: Client,
   components: UIComponent[][]
 ): MessageActionRow[] {
-  const configInRows: ButtonOptions[][] = components.map(
+  const configInRows: MessageButtonOptions[][] = components.map(
     (row) =>
       row.map((component) => {
         if (component instanceof DispatchButton) {
-          if (component.options.onClick) {
-            const id = getID(
-              component.options.label ?? '<unlabeled>',
-              'button'
-            );
-            const copy: ButtonOptions = {
-              ...component.options,
-              customId: id,
-              type: 'BUTTON',
-            };
-            delete copy.onClick;
-            client.buttonListeners.set(id, component.options.onClick);
-            return copy;
-          } else {
-            return component.options;
-          }
+          const { onClick, ...options } = component.options;
+          // nonlink buttons must have a customId
+          const id = getID(
+            component.options.label ?? '<unlabeled>',
+            'button'
+          );
+          client.buttonListeners.set(id, onClick);
+          return { ...options, type: 'BUTTON', customId: id };
+        } else if (component instanceof DispatchLinkButton) {
+          // we override style in case the user omitted it
+          return {
+            ...component.options,
+            type: 'BUTTON',
+            style: 'LINK'
+          };
         } else {
           throw new Error('No such component type!');
         }
