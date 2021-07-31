1ce08dd24283a214130a399cb3d307e6db3366af
Robert Boyd III <rob@robertcboyd.com>
2021-07-31 00:51:27 -0400

    Extract validation to functions

--- src/UI.ts
+++ src/UI.ts
@@ -97,24 +97,34 @@ export function toComponents(
     );
   // validate row constraints
   configInRows.forEach((row) => {
-    if (row.find((config) => config.type === 'SELECT_MENU') && row.length > 1) {
-      throw new Error(
-        'Rows with select menus cannot contain other elements!'
-      );
-    }
-    if (row.length > 5) {
-      throw new Error(
-        'Rows cannot have more than 5 elements!\n' +
-          // this cast should be safe as the above if covers select menus in rows
-          `Row containing "${row
-            .map((x) => (x as MessageButtonOptions).label)
-            .join(' ')}" is invalid.`
-      );
-    }
+    validateSelectMenuAlone(row);
+    validateMaxLength(row);
   });
   return configInRows.map((row) =>
     new MessageActionRow().addComponents(...row)
   );
+}
+
+function validateSelectMenuAlone(
+  row: (MessageButtonOptions | MessageSelectMenuOptions)[]
+) {
+  if (row.find((config) => config.type === 'SELECT_MENU') && row.length > 1) {
+    throw new Error('Rows with select menus cannot contain other elements!');
+  }
+}
+
+function validateMaxLength(
+  row: (MessageSelectMenuOptions | MessageSelectMenuOptions)[]
+) {
+  if (row.length > 5) {
+    throw new Error(
+      'Rows cannot have more than 5 elements!\n' +
+        // this cast should be safe as validateSelectMenuAlone covers select menus in rows
+        `Row containing "${row
+          .map((x) => (x as MessageButtonOptions).label)
+          .join(' ')}" is invalid.`
+    );
+  }
 }
 
 function getID(label: string, componentType: string): string {
