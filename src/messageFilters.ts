867926ba327647b7179942bd3144d72ee628069f
suneettipirneni <suneettipirneni@icloud.com>
2021-08-02 19:29:43 -0400

    allow filters to be async

--- src/messageFilters.ts
+++ src/messageFilters.ts
@@ -1,11 +1,12 @@
 import { Message } from 'discord.js';
 
-export type MessageFilter = (message: Message) => boolean;
+export type MessageFilter = (message: Message) => boolean | Promise<boolean>;
 
 export async function runMessageFilters(message: Message, messageFilters: MessageFilter[]): Promise<void> {
   for (const filter of messageFilters) {
     try {
-      if (!filter(message)) {
+      const filterResult = await filter(message);
+      if (!filterResult) {
         if (message.deletable) {
           await message.delete();
           return;
