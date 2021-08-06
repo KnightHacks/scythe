cb843b60a4b892e127f7ac53c4d22d041b1b20f9
Suneet Tipirneni <suneettipirneni@icloud.com>
2021-08-06 15:16:22 -0400

    add more scaffolding

--- src/__tests__/sample.test.ts
+++ src/__tests__/sample.test.ts
@@ -1,21 +1,39 @@
+import { ApplicationCommand, ClientApplication, Collection, GuildResolvable } from 'discord.js';
+import Client from '../Client';
+import { Command } from '../Command';
+
 const mockCommands: Command[] = [
   {
     name: 'Foo',
     description: 'Foo description',
-    async run(interaction) { await interaction.reply('foo'); }
+    async run({ interaction }) { await interaction.reply('foo'); }
   }
 ];
 
+const mockAppCommands: ApplicationCommand[] = mockCommands.map(mockCommand => {
+  return {
+    ...mockCommand
+  } as unknown as ApplicationCommand;
+});
+
+const appCommands = new Collection<`${bigint}`, ApplicationCommand>();
+
+mockAppCommands.forEach((mockCommand, i) => {
+  appCommands.set(`${i}` as unknown as `${bigint}`, mockCommand);
+});
+
+const altCommands = new Collection<`${bigint}`, ApplicationCommand>();
+
+altCommands.set('1', { name: 'Bar', description: 'Foo description' } as ApplicationCommand);
+
 /* eslint-disable @typescript-eslint/require-await */
-jest.mock('../loadCommands', () => ({
-  loadCommands(): Command[] {
+jest.mock('../loaders', () => ({
+  loadStructures(): Command[] {
     console.log('mocking command loads');
     return mockCommands;
   }
 }));
 
-import Client from '../Client';
-import { Command } from '../Command';
 
 let client: Client;
 beforeAll(() => {
@@ -28,15 +46,38 @@ beforeAll(() => {
     return token!;
   });
 
-  // TODO: Mock Discord.js instead of the function itself.
-  jest.spyOn(client, 'pushCommands').mockImplementation(async (_commands) => {
-    console.log('Pushing commands');
-  });
+  process.env.GUILD_ID = '1234';
 });
 
-test('Properly Pushes all Commands', async () => {
-  client.registerCommands('');
+test('Synchronization diffs properly', async () => {
+
+  client.application = new ClientApplication(client, {});
+
+  // Mock the discord api to return commands we already have.
+  jest.spyOn(client.application.commands, 'fetch')
+    .mockResolvedValueOnce(altCommands as unknown as Collection<`${bigint}`, ApplicationCommand<{ guild: GuildResolvable }>>);
+  
+  process.env.GUILD_ID = '1234';
+
+  await client.registerCommands('');
+  await client.login();
+
+  const pushSpy = jest.spyOn(client, 'pushCommands');
+
+  jest.spyOn(client, 'isReady').mockReturnValueOnce(true);
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  client.emit('ready', client);
+  
+  expect(pushSpy).toHaveBeenCalledTimes(1);
+});
+
+test.skip('Properly Pushes all Commands', async () => {
+  await client.registerCommands('');
   await client.login('foobar');
+
+  
 
   const spy = jest.spyOn(client, 'pushCommands');
 
