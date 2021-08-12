be0e88706d6f4c050278eb2886be12eedef18fad
suneettipirneni <suneettipirneni@icloud.com>
2021-08-12 16:25:30 -0400

    add type guards for command option types

--- src/utils/command.ts
+++ src/utils/command.ts
@@ -1,21 +1,25 @@
-import { ApplicationCommandOptionData, ApplicationCommandData, Constants, ChatInputApplicationCommandData, MessageApplicationCommandData, UserApplicationCommandData } from 'discord.js';
+import { ApplicationCommandOptionData, ApplicationCommandData, Constants, ChatInputApplicationCommandData, MessageApplicationCommandData, UserApplicationCommandData, ApplicationCommandChoicesData, ApplicationCommandSubGroupData } from 'discord.js';
 
 function normalizeOption(option: ApplicationCommandOptionData): ApplicationCommandOptionData {
   if (!option.required) {
     option.required = false; // Default for required
   }
-  
-  // These stubs are needed for deep comparisons
-  // because discord.js return values set to undefined.
-  if (!option.choices) {
-    option.choices = undefined;
+
+  if (isChoiceBasedOption(option)) {
+    // These stubs are needed for deep comparisons
+    // because discord.js return values set to undefined.
+    if (!option.choices) {
+      option.choices = undefined;
+    }
   }
-  
-  if (!option.options) {
-    option.options = undefined;
-  } else {
-    // Recurse through sub options.
-    option.options.map(normalizeOption);
+
+  if (isSubOptionBasedOption(option)) {
+    if (!option.options) {
+      option.options = undefined;
+    } else {
+      // Recurse through sub options.
+      option.options.map(normalizeOption);
+    }
   }
   
   return option;
@@ -31,6 +35,22 @@ export function isContextMenuCommand(commandData: ApplicationCommandData): comma
          commandData.type === 'USER' || 
          commandData.type === Constants.ApplicationCommandTypes.MESSAGE || 
          commandData.type === Constants.ApplicationCommandTypes.USER;
+}
+
+export function isChoiceBasedOption(option: ApplicationCommandOptionData): option is ApplicationCommandChoicesData {
+  return option.type === 'BOOLEAN' || 
+         option.type === Constants.ApplicationCommandOptionTypes.BOOLEAN ||
+         option.type === 'INTEGER' ||
+         option.type === Constants.ApplicationCommandOptionTypes.INTEGER ||
+         option.type === 'STRING' ||
+         option.type === Constants.ApplicationCommandOptionTypes.STRING ||
+         option.type === 'NUMBER' ||
+         option.type === Constants.ApplicationCommandOptionTypes.NUMBER;
+}
+
+export function isSubOptionBasedOption(option: ApplicationCommandOptionData): option is (ApplicationCommandSubGroupData | ApplicationCommandSubGroupData)  {
+  return option.type === 'SUB_COMMAND' ||
+         option.type === 'SUB_COMMAND_GROUP';
 }
   
 export function toData(command: ApplicationCommandData): ApplicationCommandData {
